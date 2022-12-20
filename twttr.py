6d426a9798e16eb0ebd9f7612653d1868dbe496f
Marina Saitgareeva <4006761ms@gmail.com>
2022-12-20 10:46:44 -0500

    update twttr file

--- twttr.py
+++ twttr.py
@@ -1,19 +1,18 @@
 def main():
     user_input = input("Input: ")
-    user_output = ""
-
-    for letter in user_input:
-        if letter.lower() not in ("a", "e", "i", "o", "u") and letter.upper() not in (
-            "A",
-            "E",
-            "I",
-            "O",
-            "U",
-        ):
-            # print(letter, end="")
-            user_output += letter
+    user_output = shorten(user_input)
 
     print(f"Output: {user_output}")
+
+
+def shorten(word):
+    user_output = ""
+
+    for letter in word:
+        if letter.lower() not in ("a", "e", "i", "o", "u"):
+            user_output += letter
+
+    return user_output
 
 
 if __name__ == "__main__":
